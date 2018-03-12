9ecd62a19e9681989618d63bd32fc14b4b4dc9bb
DESKTOP-CIH81L5\Thanh <phamnam1910@gmail.com>
2018-03-12 22:34:01 +0700

    Update UI

--- app/src/main/java/hvasoftware/com/thongtindoino/ui/dialog/DateSortDialog.java
+++ app/src/main/java/hvasoftware/com/thongtindoino/ui/dialog/DateSortDialog.java
@@ -1,6 +1,5 @@
 package hvasoftware.com.thongtindoino.ui.dialog;
 
-import android.app.DatePickerDialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.TextView;
+
+import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
+
+import java.util.Calendar;
 
 import hvasoftware.com.thongtindoino.R;
 
@@ -15,8 +19,12 @@ import hvasoftware.com.thongtindoino.R;
  * Created by Thanh on 03/12/2018.
  */
 
-public class DateSortDialog extends DialogFragment {
-    View wrapFrom,wrapTo;
+public class DateSortDialog extends DialogFragment implements com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener {
+    View wrapFrom, wrapTo;
+    DatePickerDialog fromDateDialog;
+    DatePickerDialog toDateDialog;
+    TextView tvFrom, tvTo;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -26,8 +34,35 @@ public class DateSortDialog extends DialogFragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Calendar calendar = Calendar.getInstance();
+        fromDateDialog = DatePickerDialog.newInstance(this, calendar);
+        toDateDialog = DatePickerDialog.newInstance(this, calendar);
         wrapFrom = view.findViewById(R.id.wrap_from);
+        tvFrom = view.findViewById(R.id.tv_from);
+        tvTo = view.findViewById(R.id.tv_to);
+        wrapFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fromDateDialog.show(getFragmentManager(), "");
+            }
+        });
         wrapTo = view.findViewById(R.id.wrap_to);
+        wrapTo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toDateDialog.show(getFragmentManager(), "");
+            }
+        });
+
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDateSet(com.wdullaer.materialdatetimepicker.date.DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        if (view == fromDateDialog) {
+            tvFrom.setText(dayOfMonth + " Tháng " + monthOfYear + 1 + " Năm " + year);
+        } else {
+            tvTo.setText(dayOfMonth + " Tháng " + monthOfYear + 1 + " Năm " + year);
+        }
     }
 }
